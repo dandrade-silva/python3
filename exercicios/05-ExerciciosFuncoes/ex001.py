d1419691adcc77ba7d648f46b6cf52a2b5a88f5f
Danilo Andrade <dandrade0803@gmail.com>
2023-08-24 00:37:10 -0300

    Exercicios de função

--- exercicios/05-ExerciciosFuncoes/ex001.py
+++ exercicios/05-ExerciciosFuncoes/ex001.py
@@ -1,5 +1,14 @@
-def ola(nome="Estudante"):
-    print(f'Olá, {nome}.')
+"""
+Faça um programa para imprimir:
+    1
+    2   2
+    3   3   3
+    .....
+    n   n   n   n   n   n  ... n 
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima (última) linha.
+"""
 
+n = int(input("Digite um número: "))
 
-ola()
+for i in range(1, n+1):
+    print(f'{i} ' * i)
--- exercicios/05-ExerciciosFuncoes/ex002.py
+++ exercicios/05-ExerciciosFuncoes/ex002.py
@@ -1,30 +1,9 @@
-# Parametros Variáveis
-
-def soma(*args):    # Por convenção, os parâmetros variaveis são chamados de args e possuem um *
-    total = 0
-    for valor in args:
-        total += valor
-    print(f'A soma dos números é igual a {total}')
-
-
-def f(**kwargs):
-    # Por convenção, os parâmetros variaveis do TIPO "Nomeado" são chamados de kargs e possuem dois **
-    # Ele é do tipo dicionário, por isso, só aceita receber parâmetros nomeados, por exemplo: nome = "Danilo"
-    print(kwargs)
-    print(type(kwargs))
-
-
-def f2(*args, **kwargs):
-    # Também podemos combinar args e kwargs
-    print(args)
-    print(kwargs)
-
-
-# f2(args=(1, 2, 3), nome='Danilo', idade=3)
-
-# Para passar uma lista de valores na função f2, temos que realizar o despacotamento.
-# Segue um exemplo
-args = (2, 4, 10, 20)
-kwargs = {'nome': 'Danilo', 'idade': 30}
-
-f2(*args, **kwargs)
+"""
+Faça um programa para imprimir:
+    1
+    1   2
+    1   2   3
+    .....
+    1   2   3   ...  n
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima (última) linha.
+"""
